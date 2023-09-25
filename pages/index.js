81aee5feeea559190fe20f64cd3723bd3c99eff3
Jahid Hasan <jahidhasan2856@gmail.com>
2023-09-26 00:22:18 +0600

    adding how works section

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
       </div>
 
       <div
-        className="px-5 relative top-[-135px] xl:px-0 mt-16  py-20 after:absolute after:w-full after:h-full after:inset-0
+        className="px-5 relative top-[-100px] xl:px-0 mt-16  py-20 after:absolute after:w-full after:h-full after:inset-0
          after:bg-black/10 -z-[11] after:-z-10"
         style={{
           background: "url(/images/5348666.jpg)",
@@ -74,7 +74,7 @@ export default function Home() {
             <InsuranceSolutions
               image={"/images/dollar-symbol.png"}
               countNumber={0}
-              suffix={''}
+              suffix={""}
               title={"Investment"}
             />
             <InsuranceSolutions
@@ -98,24 +98,42 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="">
+        <div className="container mx-auto">
+          <h2
+            className="text-black p-[0.40rem] mb-2 text-center text-4xl font-bold
+       "
+          >
+            <span className=" relative after:absolute after:w-full after:h-[2px] after:rounded-full after:bg-orange-500 after:left-0 after:-bottom-1">
+              How Insurance Buzz Works?
+            </span>
+          </h2>
+          <p className="max-w-5xl mx-auto mt-6 text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
       <div
-        className="px-5 relative top-[-100px] xl:px-0 mt-16  py-32 after:absolute after:w-full after:h-full after:inset-0
+        className="px-5  xl:px-0 my-16  py-32 after:absolute after:w-full after:h-full after:inset-0
          after:bg-black/10 -z-[11] after:-z-10"
         style={{
           background: "url(/images/happy-family.jpg)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-         
         }}
       >
-        <div
-
-          className=" px-5 py-10  xl:max-w-7xl max-w-screen-lg rounded bg-back backdrop-blur-sm border border-blue-500 bg-white/75 m-auto opacity-100 z-10"
-        >
+        <div className=" px-5 py-10  xl:max-w-7xl max-w-screen-lg rounded bg-back backdrop-blur-sm border border-blue-500 bg-white/75 m-auto opacity-100 z-10">
           <div className="text-center">
             <h2 className="text-[46px] font-bold">1,254</h2>
             <h4 className="text-xl font-bold">Canadians</h4>
-            <p className="text-base text-gray-600 mt-1">have compared rates and saved money <b>over the last 24 hours</b></p>
+            <p className="text-base text-gray-600 mt-1">
+              have compared rates and saved money <b>over the last 24 hours</b>
+            </p>
           </div>
         </div>
       </div>
