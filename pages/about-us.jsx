697a1ff2d62985d03b48051c327c2f9e410b16f5
pronazmul <developernazmul@gmail.com>
2023-10-08 11:28:35 +0600

    Ui Update, About Us Page & Resource Page

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -1,129 +1,130 @@
 // import MainNav from '@/components/NormalNav/MainNav';
 // import TopNav from '@/components/NormalNav/TopNav';
-import GoalsAndMissions from "@/components/Goals/GoalsAndMissions";
-import GoalsBox from "@/components/Goals/GoalsBox";
-import Process from "@/components/Process/Process";
-import ProcessBox from "@/components/Process/ProcessBox";
-import React, { useEffect, useState } from "react";
+import GoalsAndMissions from '@/components/Goals/GoalsAndMissions'
+import GoalsBox from '@/components/Goals/GoalsBox'
+import Process from '@/components/Process/Process'
+import ProcessBox from '@/components/Process/ProcessBox'
+import React, { useEffect, useState } from 'react'
 
 const data = [
   {
     id: 1,
-    title: "",
-    text: "We’re focused on changing the way Canadians buy insurance. In doing so, we affirm to certain principles:",
+    title: '',
+    text: 'We’re focused on changing the way Canadians buy insurance. In doing so, we affirm to certain principles:',
   },
   {
     id: 2,
-    title: "We put our customer first.",
-    text: "We will make your insurance buying experience as seamless and pleasant as we can.",
+    title: 'We put our customer first.',
+    text: 'We will make your insurance buying experience as seamless and pleasant as we can.',
   },
   {
     id: 3,
-    title: "We keep it simple.",
-    text: "Enough said.",
+    title: 'We keep it simple.',
+    text: 'Enough said.',
   },
   {
     id: 4,
-    title: "We’re always honest.",
-    text: "Our advisors are salary-based and do not earn commissions. We’re on Team You and will only offer advice that is in your best interests. You have no obligation to buy anything if we don’t find a product at a price that you’re satisfied with.",
+    title: 'We’re always honest.',
+    text: 'Our advisors are salary-based and do not earn commissions. We’re on Team You and will only offer advice that is in your best interests. You have no obligation to buy anything if we don’t find a product at a price that you’re satisfied with.',
   },
   {
     id: 5,
-    title: "We’re always available.",
-    text: "We are always online. If you have questions about anything at all, we will do our best to provide you with answers no matter what time you’re asking, no appointment necessary.",
+    title: 'We’re always available.',
+    text: 'We are always online. If you have questions about anything at all, we will do our best to provide you with answers no matter what time you’re asking, no appointment necessary.',
   },
   {
     id: 6,
-    title: "We only work with the best.",
-    text: "We partner exclusively with the most trusted insurance companies in Canada, that are leaders in the protection industry.",
+    title: 'We only work with the best.',
+    text: 'We partner exclusively with the most trusted insurance companies in Canada, that are leaders in the protection industry.',
   },
   {
     id: 7,
-    title: "We protect your privacy.",
-    text: "Your security is our priority and your data is safe with us. We won’t sell your info to third parties.",
+    title: 'We protect your privacy.',
+    text: 'Your security is our priority and your data is safe with us. We won’t sell your info to third parties.',
   },
-];
+]
 
 const AboutUsPage = () => {
-  const [sticky, setSticky] = useState(false);
+  const [sticky, setSticky] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY >= 100) {
-        setSticky(true);
+        setSticky(true)
       } else {
-        setSticky(false);
+        setSticky(false)
       }
-    };
+    }
 
     // Add the scroll event listener
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     // Clean up the event listener when the component unmounts or route changes
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
     <div>
-
-    <div
-      className=" relative top-[-165px]"
-      style={{
-        background: `url("/images/new/team-trust.jpg")`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        objectFit: "cover",
-      }}
-    >
-      <div className="pt-60 pb-20 text-white bg-[#3d435db0]">
-        <div className="max-w-7xl mx-auto  px-4 lg:px-0">
-          <h2
-            className="text-white p-[0.40rem] mb-2 text-center text-4xl font-bold
-       "
-          >
-            <span className=" relative after:absolute after:w-full after:h-[2px] after:rounded-full after:bg-orange-500 after:left-0 after:-bottom-1">
-              Our story
-            </span>
-          </h2>
-          <div className="max-w-4xl mx-auto mt-5 text-justify text-base leading-8">
-            Connecting People Through Innovation The inception of InsuranceBuzz
-            was rooted in a profound realization - the traditional insurance
-            industry, with its complexities and rigid structures, often left
-            people feeling disconnected and underserved. We understood that
-            insurance isn't just about policies and premiums; it's about
-            safeguarding what truly matters in life. With this ethos, we
-            embarked on a journey to revolutionize the insurance sector. Our
-            story begins with a passionate team of visionaries who believed in
-            the power of connectivity. We saw an opportunity to bridge the gap
-            between insurance providers and the individuals they serve.
-            <br />
-            <br />
-            InsuranceBuzz was born as a platform that doesn't merely offer
-            information; it forges meaningful connections. We set out to create
-            a space where people could find not only insurance insights but also
-            a sense of community, support, and empowerment. Our innovative
-            approach aims to humanize the insurance experience, making it more
-            accessible, transparent, and ultimately, about the people it serves.
-            At InsuranceBuzz, we believe that by connecting individuals with the
-            right knowledge and resources, we can help them navigate the
-            insurance landscape with confidence, transforming a daunting process
-            into a journey of protection and peace of mind.
+      <div
+        className=' relative top-[-165px]'
+        style={{
+          background: `url("/images/new/our_story-transformed.jpeg")`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          objectFit: 'cover',
+        }}
+      >
+        <div className='pt-60 pb-20 text-white bg-[#3d435db0]'>
+          <div className='max-w-7xl mx-auto  px-4 lg:px-0'>
+            <h2
+              className='text-white p-[0.40rem] mb-2 text-center text-4xl font-bold
+       '
+            >
+              <span className=' relative after:absolute after:w-full after:h-[2px] after:rounded-full after:bg-orange-500 after:left-0 after:-bottom-1'>
+                Our story
+              </span>
+            </h2>
+            <div className='max-w-4xl mx-auto mt-5 text-justify text-base leading-8'>
+              Connecting People Through Innovation The inception of
+              InsuranceBuzz was rooted in a profound realization - the
+              traditional insurance industry, with its complexities and rigid
+              structures, often left people feeling disconnected and
+              underserved. We understood that insurance isn't just about
+              policies and premiums; it's about safeguarding what truly matters
+              in life. With this ethos, we embarked on a journey to
+              revolutionize the insurance sector. Our story begins with a
+              passionate team of visionaries who believed in the power of
+              connectivity. We saw an opportunity to bridge the gap between
+              insurance providers and the individuals they serve.
+              <br />
+              <br />
+              InsuranceBuzz was born as a platform that doesn't merely offer
+              information; it forges meaningful connections. We set out to
+              create a space where people could find not only insurance insights
+              but also a sense of community, support, and empowerment. Our
+              innovative approach aims to humanize the insurance experience,
+              making it more accessible, transparent, and ultimately, about the
+              people it serves. At InsuranceBuzz, we believe that by connecting
+              individuals with the right knowledge and resources, we can help
+              them navigate the insurance landscape with confidence,
+              transforming a daunting process into a journey of protection and
+              peace of mind.
+            </div>
           </div>
         </div>
-      </div>
       </div>
 
       <div
         style={{
-          background: `url(${"/images/sub/mission.jpg"})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+          background: `url(${'/images/sub/mission.jpg'})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
         }}
-        className="relative top-[-165px]"
+        className='relative top-[-165px]'
       >
         <GoalsAndMissions />
       </div>
@@ -135,23 +136,22 @@ const AboutUsPage = () => {
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
-        className="relative top-[-165px]"
+        className='relative top-[-165px]'
       >
-             <Process />
-
+        <Process />
       </div>
 
-      <div className="bg-white pt-20 relative top-[-165px]">
-        <div className="max-w-7xl mx-auto  px-4 lg:px-0">
+      <div className='bg-white pt-20 relative top-[-165px]'>
+        <div className='max-w-7xl mx-auto  px-4 lg:px-0'>
           <h2
-            className="text-black p-[0.40rem] mb-2 text-center text-4xl font-bold
-       "
+            className='text-black p-[0.40rem] mb-2 text-center text-4xl font-bold
+       '
           >
-            <span className=" relative after:absolute after:w-full after:h-[2px] after:rounded-full after:bg-orange-500 after:left-0 after:-bottom-1">
+            <span className=' relative after:absolute after:w-full after:h-[2px] after:rounded-full after:bg-orange-500 after:left-0 after:-bottom-1'>
               Our Team
             </span>
           </h2>
-          <div className="max-w-4xl mx-auto mt-10 text-justify text-base leading-8">
+          <div className='max-w-4xl mx-auto mt-10 text-justify text-base leading-8'>
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
@@ -167,10 +167,8 @@ const AboutUsPage = () => {
           </div>
         </div>
       </div>
-   
     </div>
+  )
+}
 
-  );
-};
-
-export default AboutUsPage;
+export default AboutUsPage
--- pages/resources.js
+++ pages/resources.js
@@ -1,191 +1,222 @@
-import { Menu } from "antd";
+import { Menu } from 'antd'
 
-import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { HeroSection } from '@/components/HeroSection/HeroSection'
 
-import { useRouter, useSearchParams, usePathname } from "next/navigation";
-import LifeInsurance from "@/components/Resources/LifeInsurance";
-import CriticalInsurance from "@/components/Resources/CriticalInsurance";
-import DisabilityInsurance from "@/components/Resources/DisabilityInsurance";
-import HelthInsurance from "@/components/Resources/HelthInsurance";
-import TermInsurance from "@/components/Resources/TermInsurance";
-import TravelInsurance from "@/components/Resources/TravelInsurance";
-import IntStudentInsurance from "@/components/Resources/IntStudentInsurance";
-import SuperVisaInsurance from "@/components/Resources/SuperVisaInsurance";
-import RESP from "@/components/Resources/RESP";
-import RRSP from "@/components/Resources/RRSP";
-import TFSA from "@/components/Resources/TFSA";
-import ImportantLinks from "@/components/Resources/ImportantLinks";
-import { useEffect, useRef, useState } from "react";
+import { useRouter, useSearchParams, usePathname } from 'next/navigation'
+import LifeInsurance from '@/components/Resources/LifeInsurance'
+import CriticalInsurance from '@/components/Resources/CriticalInsurance'
+import DisabilityInsurance from '@/components/Resources/DisabilityInsurance'
+import HelthInsurance from '@/components/Resources/HelthInsurance'
+import TermInsurance from '@/components/Resources/TermInsurance'
+import TravelInsurance from '@/components/Resources/TravelInsurance'
+import IntStudentInsurance from '@/components/Resources/IntStudentInsurance'
+import SuperVisaInsurance from '@/components/Resources/SuperVisaInsurance'
+import RESP from '@/components/Resources/RESP'
+import RRSP from '@/components/Resources/RRSP'
+import TFSA from '@/components/Resources/TFSA'
+import ImportantLinks from '@/components/Resources/ImportantLinks'
+import { useEffect, useRef, useState } from 'react'
 
 const items = [
   {
-    key: "life-insurance",
-    href: "#life-insurance",
-    label: "Life Insurance",
+    key: 'life-insurance',
+    href: '#life-insurance',
+    label: 'Life Insurance',
     children: [
       {
-        key: "critical-insurance",
-        href: "#critical-insurance",
-        label: "Critical Illness Insurance",
+        key: 'term-insurance',
+        href: '#term-insurance',
+        label: 'Term Life Insurance',
       },
       {
-        key: "disability-insurance",
-        href: "#disability-insurance",
-        label: "Disability Insurance",
+        key: 'universal-life-insurance',
+        href: '#universal-life-insurance',
+        label: 'Universal Life Insurance',
       },
       {
-        key: "health-insurance",
-        href: "#health-insurance",
-        label: "Health Insurance",
+        key: 'whole-life-insurance',
+        href: '#whole-life-insurance',
+        label: 'Whole Life Insurance',
+      },
+    ],
+  },
+  {
+    key: 'critical-insurance',
+    href: '#critical-insurance',
+    label: 'Critical Illness Insurance',
+  },
+  {
+    key: 'disability-insurance',
+    href: '#disability-insurance',
+    label: 'Disability Insurance',
+  },
+  {
+    key: 'health-insurance',
+    href: '#health-insurance',
+    label: 'Health Insurance',
+  },
+  {
+    key: 'travel-insurance',
+    href: '#travel-insurance',
+    label: 'Travel Insurance',
+    children: [
+      {
+        key: 'visitor-to-canada-insurance',
+        href: '#visitor-to-canada-insurance',
+        label: 'Visitor To Canada Insurance',
+      },
+      {
+        key: 'supervisa-insurance',
+        href: '#supervisa-insurance',
+        label: 'Super Visa Insurance',
+      },
+      {
+        key: 'travel-insurance',
+        href: '#travel-insurance',
+        label: 'Travel Insurance',
+      },
+      {
+        key: 'international-students-insurance',
+        href: '#international-insurance',
+        label: 'International Students Insurance',
+      },
+    ],
+  },
+  {
+    key: 'auto-insurance',
+    href: '#auto-insurance',
+    label: 'Auto Insurance',
+    children: [],
+  },
+  {
+    key: 'home-insurance',
+    href: '#home-insurance',
+    label: 'Home Insurance',
+    children: [],
+  },
+  {
+    key: 'investments',
+    href: '#investments',
+    label: 'Investments',
+    children: [
+      {
+        key: 'rrsp',
+        href: '#rrsp',
+        label: 'RRSP',
+      },
+      {
+        key: 'tfsa',
+        href: '#tfsa',
+        label: 'TFSA',
+      },
+      {
+        key: 'resp',
+        href: '#resp',
+        label: 'RESP',
       },
     ],
   },
 
   {
-    key: "term-insurance",
-    href: "#term-insurance",
-    label: "Term Life Insurance",
+    key: 'claim-process',
+    href: '#claim-process',
+    label: 'Claims Process',
   },
   {
-    key: "travel-insurance",
-    href: "#travel-insurance",
-    label: "Travel Insurance",
-    children: [
-      {
-        key: "international-students-insurance",
-        href: "#international-insurance",
-        label: "International Students Insurance",
-      },
-      {
-        key: "supervisa-insurance",
-        href: "#supervisa-insurance",
-        label: "Super Visa Insurance",
-      },
-    ],
-  },
-
-  {
-    key: "investments",
-    href: "#investments",
-    label: "Investments",
-    children: [
-      {
-        key: "resp",
-        href: "#resp",
-        label: "RESP",
-      },
-      {
-        key: "rrsp",
-        href: "#rrsp",
-        label: "RRSP",
-      },
-      {
-        key: "tfsa",
-        href: "#tfsa",
-        label: "TFSA",
-      },
-    ],
-  },
-
-  {
-    key: "claim-insurance",
-    href: "#claim-insurance",
-    label: "Claim Process",
+    key: 'claim-insurance',
+    href: '#claim-insurance',
+    label: 'Claim Contact Numbers',
   },
   {
-    key: "important-links",
-    href: "#important-links",
-    label: "Important Links",
+    key: 'important-links',
+    href: '#important-links',
+    label: 'Important Links',
   },
-];
+]
 
 const ResourcesPage = () => {
-  const [current, setCurrent] = useState("life-insurance");
-  const [collapsed, setCollapsed] = useState(false);
-  const router = useRouter();
-  const pathname = usePathname();
+  const [current, setCurrent] = useState('life-insurance')
+  const [collapsed, setCollapsed] = useState(false)
+  const router = useRouter()
+  const pathname = usePathname()
 
-  const searchParams = useSearchParams();
-  const tab = searchParams.get("tab");
-  const ref = useRef();
-
+  const searchParams = useSearchParams()
+  const tab = searchParams.get('tab')
+  const ref = useRef()
 
   useEffect(() => {
     console.log(ref.current.offsetTop)
-    if(tab){
-      window.scrollTo(0, ref.current.offsetTop - 120);
+    if (tab) {
+      window.scrollTo(0, ref.current.offsetTop - 120)
     }
-  
   }, [tab])
 
   const onClick = (e) => {
-    router.push({ href: pathname, query: { tab: e.key } },  undefined, { scroll: false } );
-    setCurrent(e.key);
-
-  };
+    router.push({ href: pathname, query: { tab: e.key } }, undefined, {
+      scroll: false,
+    })
+    setCurrent(e.key)
+  }
 
   const renderSection = (params) => {
     switch (params) {
-      case "life-insurance":
-        return <LifeInsurance />;
-      case "critical-insurance":
-        return <CriticalInsurance />;
-      case "disability-insurance":
-        return <DisabilityInsurance />;
-      case "health-insurance":
-        return <HelthInsurance />;
-      case "term-insurance":
-        return <TermInsurance />;
-      case "travel-insurance":
-        return <TravelInsurance />;
-      case "international-students-insurance":
-        return <IntStudentInsurance />;
-      case "supervisa-insurance":
-        return <SuperVisaInsurance />;
-      case "resp":
-        return <RESP />;
-      case "rrsp":
-        return <RRSP />;
-      case "tfsa":
-        return <TFSA />;
-      case "claim-insurance":
-        return <LifeInsurance />;
-      case "important-links":
-        return <ImportantLinks />;
+      case 'life-insurance':
+        return <LifeInsurance />
+      case 'critical-insurance':
+        return <CriticalInsurance />
+      case 'disability-insurance':
+        return <DisabilityInsurance />
+      case 'health-insurance':
+        return <HelthInsurance />
+      case 'term-insurance':
+        return <TermInsurance />
+      case 'travel-insurance':
+        return <TravelInsurance />
+      case 'international-students-insurance':
+        return <IntStudentInsurance />
+      case 'supervisa-insurance':
+        return <SuperVisaInsurance />
+      case 'resp':
+        return <RESP />
+      case 'rrsp':
+        return <RRSP />
+      case 'tfsa':
+        return <TFSA />
+      case 'claim-insurance':
+        return <LifeInsurance />
+      case 'important-links':
+        return <ImportantLinks />
       default:
-        return <LifeInsurance />;
+        return <LifeInsurance />
     }
-  };
-
+  }
 
   return (
     <>
       <HeroSection
-        heading={"Frequently Asked Questions"}
+        heading={'Frequently Asked Questions'}
         para={`Welcome to the hub of frequently asked questions on insurance.`}
-        img={"/images/sub/FAQ-hero-image.jpeg"}
-        color={"#5393becc"}
-        position="top"
+        img={'/images/sub/FAQ-hero-image.jpeg'}
+        color={'#5393becc'}
+        position='top'
       />
-      <div className="bg-[#305fa9] relative left-0 right-0 top-[-165px]" ref={ref}>
-        <div className="grid grid-cols-6">
+      <div className='bg-[#305fa9] mt-[-165px]' ref={ref}>
+        <div className='grid grid-cols-6'>
           <Menu
             onClick={onClick}
             selectedKeys={[current]}
-            mode="inline"
+            mode='inline'
             items={items}
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100vh" }}
-            theme="dark"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100vh' }}
+            theme='dark'
           />
-          <div className="col-span-5  container mr-20">
-            {renderSection(tab)}
-          </div>
+          <div className='col-span-5 container'>{renderSection(tab)}</div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ResourcesPage;
+export default ResourcesPage
